e58b2d4c663dd247917ff7bb0702c1bdde7bce03
gopireddy02637@gmail.com <bredd@vestas.com>
2024-05-05 07:48:36 +0530

    sestsokopt setting the socket for port with error

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ server handleing single client and server Handling multiple cilents
 more on theory of socket programming (TCP/IP,UDP and IPV4,IPv6)
 
 
-##NOTE1
+## NOTE1
 Socket
 it is nothing but file descriptor, just like we have file open and file read in c prog it returns pointer but here it returne integer vaule.
 it have 2 programs server and cilent 
@@ -31,14 +31,14 @@ Datagram Socket (SOCK_DGRAM):
 cONNECTIONLESS SOCKETS. uSES udp .not reliable.Packet may/may not reach but if reache ,will be error free . uses ip for routing
 Used when TCP stack is either not available or few dropped packets doesn't harm(like video confrencing , movies etc)
 
-###Note2
+### Note2
 Big endian(Network Byte Order) // litle endian byte order:
 Big endian - the most significant bit ( the bit end )(0x12345678) is placed at the byte with the lowerst address. the rest of the data is placed in the orde on the three bytes in memory.
 Little Endian byte order: the least signifiacnt byte ( the little end )(0x78563412)of thr data is placed at the byte with the lowest address. the rest od the data placed in order intthe nest three bytes in memeory
 in netwrok we used it big ending
 APIs (htons(sending),ntohs(receving)) conversion to and from network to host byte orders.
 
-###Note3
+### Note3
 Blocking and Non Blocking
 Blocking socket is set a request and is waiting for server respons and wait until it get it so for that 
 the create multi threding concept 
@@ -49,3 +49,17 @@ alway run a thread never block read and erite  from buffer to socket channel
 we have ioctlsocket(SOCKET s,long cmd,u_long *argv) thisis API which help blcoking ang non blocking socket succes 1 unsucce -1
 u_long val = 0 for blocking 1 for Non-blocking we are using blocking socket becoz we use multithreading 
 by default evrything is blocking socket 
+
+
+#### Note 4
+setsocketopt call
+call this before bind funtion , only one use of each socket addres(protocol/network/port)
+int setsockopt(SOCKET s,int level,int optname,const char *optval,int optlen)
+level= options for socket descrpter
+optname  =name of the socket, allow to use same prot which my socket is using , tis will allow if some other port is using the same port
+optval , optlen are for blocking or non blocking 
+retrun 0 if succescussfull
+now we will see how the new request are handle but befoer that we need to write cilent prog what if cilent connect and how it communicTE TO YOU
+
+##### nOTE 5
+CILENT Progamming
---
+++ setsockopt.cpp
@@ -0,0 +1,118 @@
+
+
+#include<iostream>
+#include<winsock.h>
+
+using namespace std:
+
+int nMaxFD 
+#define PORT 9960
+//scoket addres details as a varable i am taking
+struct sockaddr_in srv;
+//file_destructer no. of socket destructer or do some read ,write execpetio ]n 64 FD_SETSIZE FILE DESTRUCTED U CAN DD
+
+fd_set fr,fw,fe;
+
+int main()
+{
+    int nRET= 0;
+// initialize the wsa Variables (suppporting socket prog in winodws platform)
+WSADATA ws;
+if(WSAStartup(MAKEWORD(2,2),&ws)<0)
+{
+    cout<<endl<<"WSA Failed to init";
+}
+else
+{
+    cout<<endl<<"WSA successfully";
+}
+//initialze the socket 
+int nsocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+if (nsocket<0)
+{
+    cout <<endl <<"the socket not opend";
+    //print socket id
+    cout<<endl<<nsocket;//184
+}
+else{
+    cout<<endl<<"opended successfully"
+}
+//initiae the environment for sockaddr structure
+srv.sin_family = AF_INET;
+srv.sin_port = htons(PORT);
+//srv.sin_addr.s_addr = inet_addr("127.0.0.1");
+srv.sin_addr.s_addr = INADDR_ANY;
+memset(&(srv.sin_zero),0,8);
+
+//set socket opting 
+int optval = 0;
+int optlen = sizeof(optval);
+nRET=setsockopt(nsocket,SOL_SOCKET,SO_REUSEADDR,(const char*)&optval,optlen);
+if(!nRET)
+{
+    cout<<"this is succesull setsockopt";
+}
+else
+{
+    cout<<"this is failed setcokport";
+}
+//bind the socket to local port
+
+nRET = bind(nsocket,(sockaddr*)&srv,sizeof(sockaddr));
+if(nRET<0)
+{
+    cout<<endl<<"failed to bind to local port";
+}
+else
+{
+    cout<<endl<<"successfully binded the local port";
+}
+//Listen the request form client (queues the requests)
+nRET = listen(nsocket,5);
+if(nRET<0)
+[
+    cout<<endl<<"fail to start listen to local port"
+]
+else
+{
+    cout<<endl<<"started listening to local port"
+}
+//keep wait for new requests and processd as per the request
+nMaxFD = nsocket;
+struct timeval tv;
+//waiting for 1 sec to read ,write ,get exeception by setting the tv before that need to set
+v.tv_sec = 1;
+tc.tv_usec =0;
+//keep waiting until any connection comes
+while(1)
+{
+// making as zero init then reading write it
+FD_ZERO(&fr);
+FD_ZERO(&fw);
+FD_ZERO(&fe);
+
+FD_SET(Nsocket,&fr);
+FD_SET(nSocket,&fe);
+
+cout<<endl<<"before select call:"<<fr.fd_count;
+//int select(int nfds,fd_set *readfds,fd_set *writefds,fd_set *exceptions,const timeval *timeout)
+//it tells the number of FDs are there
+nRET= Select(nmazfd+1,&fr,&fw,&fe,&tv);
+if(nret>0)
+{
+    //when someone connect or communicate with a message over a dedicate connection // number of socket FD to communiccate
+    cout<<
+}
+else if (nret == 0)
+{
+    //no connection or no communication request made or you can say none of the socket descruted ready
+}
+else
+{
+    //if -1 it failed anf your applicartion shpould show soem useful message 
+    exit(-1)
+}
+cout<<endl<<"after select call:"<<fr.fd_count;
+}
+retrun 0;
+}
